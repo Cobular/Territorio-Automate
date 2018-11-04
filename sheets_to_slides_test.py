439cefdb7617c638ebc3f64e379d9a47f71a6f06
Jake Cover <coverj715@gmail.com>
2018-11-04 20:35:48 +0000

    Can now return the text and ID of all textboxes. Will be putting this into a dict and have the spreadsheet's results reference the dict. This should save a lot of time and be more practical since I won't have to worry about the other text boxes as much.

--- sheets_to_slides_test.py
+++ sheets_to_slides_test.py
@@ -3,6 +3,8 @@ from __future__ import print_function
 from apiclient import discovery
 from httplib2 import Http
 from oauth2client import file, client, tools
+import json
+
 
 # User defined variables
 sheetID = '1e8kHX4WTiOcxU_BPcIgjcSjcmVhkzLBITtXVyGZJ8lU'  # <--- Put the id of the sheet there
@@ -10,6 +12,7 @@ presentationID = '1QFH68wLkhyfVag4c2JJ1tcEhgSIXhP5W0x-4vRbRrNY'  # <--- Put the 
 dataRange = 'A2:E4'  # <--- Put the range of the data to process here. Currently only works for one sheet tab at a time
 
 
+# A ton of Authentication stuff from here to the next comment. Imporatnt but not very interesting
 SCOPES = (
     'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/presentations'
@@ -23,10 +26,24 @@ HTTP = creds.authorize(Http())
 SLIDES = discovery.build('slides', 'v1', http=HTTP)
 SHEETS = discovery.build('sheets', 'v4', http=HTTP)
 
+# Actually get the sheet and slides values data from the API. Returns a JSON file for each one.
 print('** Fetching Data, may take a bit')
 requestedSheetValues = SHEETS.spreadsheets().values().get(range=dataRange,
-                                            spreadsheetId=sheetID).execute().get('values')
+                                                          spreadsheetId=sheetID).execute().get('values')
 slideValuesTest = SLIDES.presentations().get(presentationId=presentationID).execute().get('slides')
-print(requestedSheetValues)
-print(slideValuesTest[:1])
-# for row in requestedSheetValues:
+
+
+
+counter = 0
+for textboxes in slideValuesTest[0]['pageElements']:
+    #if not textboxes['shape']:
+  #      print("Not a shape!")
+  #  else:
+    try:
+        print(textboxes['shape']['text']['textElements'][1]['textRun']['content'])
+        print(textboxes['objectId'])
+        counter += 1
+    except KeyError as error:
+        print('Not a Shape! ' + str(error))
+
+print(counter)
