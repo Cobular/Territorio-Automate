9d1f761ecfb46fd481cbb0f2b2e0b3b1e2ee8456
Jake Cover <coverj715@gmail.com>
2018-11-04 21:09:40 +0000

    Can now load up the spreadsheet parts I need

--- sheets_to_slides_test.py
+++ sheets_to_slides_test.py
@@ -5,11 +5,13 @@ from httplib2 import Http
 from oauth2client import file, client, tools
 import json
 
+# Assorted Variables
+textbox_reference = {}
 
 # User defined variables
-sheetID = '1e8kHX4WTiOcxU_BPcIgjcSjcmVhkzLBITtXVyGZJ8lU'  # <--- Put the id of the sheet there
+sheetID = '1TcS0U8AdKQGcmAO1Hf4S8hrEs7BQ1aRqzOWFCx6o_ok'  # <--- Put the id of the sheet there
 presentationID = '1QFH68wLkhyfVag4c2JJ1tcEhgSIXhP5W0x-4vRbRrNY'  # <--- Put the id of the presentation
-dataRange = 'A2:E4'  # <--- Put the range of the data to process here. Currently only works for one sheet tab at a time
+dataRange = 'A2:I172'  # <--- Put the range of the data to process here. Currently only works for one sheet tab at a time
 
 
 # A ton of Authentication stuff from here to the next comment. Imporatnt but not very interesting
@@ -26,24 +28,24 @@ HTTP = creds.authorize(Http())
 SLIDES = discovery.build('slides', 'v1', http=HTTP)
 SHEETS = discovery.build('sheets', 'v4', http=HTTP)
 
-# Actually get the sheet and slides values data from the API. Returns a JSON file for each one.
+# Actually get the sheet and slides values data from the API. Returns a JSON     file for each one.
 print('** Fetching Data, may take a bit')
-requestedSheetValues = SHEETS.spreadsheets().values().get(range=dataRange,
-                                                          spreadsheetId=sheetID).execute().get('values')
-slideValuesTest = SLIDES.presentations().get(presentationId=presentationID).execute().get('slides')
+requestedSheetValues = SHEETS.spreadsheets().values().get(range=dataRange, spreadsheetId=sheetID,
+                                                          majorDimension='COLUMNS').execute().get('values')
+requestedSlideValues = SLIDES.presentations().get(presentationId=presentationID).execute().get('slides')
 
 
-
-counter = 0
-for textboxes in slideValuesTest[0]['pageElements']:
-    #if not textboxes['shape']:
-  #      print("Not a shape!")
-  #  else:
+# This iterates over the list of page elements and only adds elements that have a text component to the
+# textbox_reference dict, which will be compared to the spreadsheet later
+for textboxes in requestedSlideValues[0]['pageElements']:
     try:
-        print(textboxes['shape']['text']['textElements'][1]['textRun']['content'])
-        print(textboxes['objectId'])
-        counter += 1
-    except KeyError as error:
-        print('Not a Shape! ' + str(error))
+        textbox_reference.update(
+            {textboxes['objectId']: textboxes['shape']['text']['textElements'][1]['textRun']['content']}
+        )
+    except KeyError:
+        pass
+print(textbox_reference)
 
-print(counter)
+# Iterate over the sheet and
+with open('sheetsData.json', 'w') as sheetsDataFile:
+    json.dump(requestedSheetValues, sheetsDataFile, indent=4)
