3bcf3fb41a791d74c79f079e3b051ae90f9db362
Jake Cover <coverj715@gmail.com>
2018-11-05 21:04:44 +0000

    The savage and blighted effects are applied, but don't stack

--- sheets_to_slides_test.py
+++ sheets_to_slides_test.py
@@ -8,6 +8,21 @@ import json
 # Assorted Variables
 textbox_reference = {}
 
+# Random functions I use
+'''
+Replace a set of multiple sub strings with a new string in main string.
+'''
+def replaceMultiple(mainString, toBeReplaces, newString):
+    # Iterate over the strings to be replaced
+    for elem in toBeReplaces:
+        # Check if string is in the main string
+        if elem in mainString:
+            # Replace the string
+            mainString = mainString.replace(elem, newString)
+
+    return mainString
+
+
 # User defined variables
 sheetID = '1TcS0U8AdKQGcmAO1Hf4S8hrEs7BQ1aRqzOWFCx6o_ok'  # <--- Put the id of the sheet there
 presentationID = '1QFH68wLkhyfVag4c2JJ1tcEhgSIXhP5W0x-4vRbRrNY'  # <--- Put the id of the presentation
@@ -28,10 +43,11 @@ HTTP = creds.authorize(Http())
 SLIDES = discovery.build('slides', 'v1', http=HTTP)
 SHEETS = discovery.build('sheets', 'v4', http=HTTP)
 
+
 # Actually get the sheet and slides values data from the API. Returns a JSON     file for each one.
 print('** Fetching Data, may take a bit')
 requestedSheetValues = SHEETS.spreadsheets().values().get(range=dataRange, spreadsheetId=sheetID,
-                                                          majorDimension='COLUMNS').execute().get('values')
+                                                          majorDimension='ROWS').execute().get('values')
 requestedSlideValues = SLIDES.presentations().get(presentationId=presentationID).execute().get('slides')
 
 
@@ -40,12 +56,111 @@ requestedSlideValues = SLIDES.presentations().get(presentationId=presentationID)
 for textboxes in requestedSlideValues[0]['pageElements']:
     try:
         textbox_reference.update(
-            {textboxes['objectId']: textboxes['shape']['text']['textElements'][1]['textRun']['content']}
+            {str(textboxes['shape']['text']['textElements'][1]['textRun']['content']).strip().lower():
+             str(textboxes['objectId']).strip().lower()}
         )
     except KeyError:
         pass
 print(textbox_reference)
 
-# Iterate over the sheet and
 with open('sheetsData.json', 'w') as sheetsDataFile:
     json.dump(requestedSheetValues, sheetsDataFile, indent=4)
+
+# Creates an empty list to reduce the number of requests sent.
+requests = []
+# Iterate over the sheet and update the slide to match it.
+# Lots of JSON POST request bodies, so it's pretty messy-looking. Just collapse the requests.
+for row in requestedSheetValues:
+    # Sets to false if anything is set to the tile.
+    # If the tile is normal, this stays true and is used to set the tile to default at the end
+    default = True
+    # Check to make sure that name of the row corresponds to an ID
+    if textbox_reference.get(str(row[0]).strip().lower()) is not None:
+        # Control blighted tiles
+        if row[5] == 'Blighted':
+            default = False
+            requests.append([
+                {
+                    "updateTextStyle": {
+                        "style": {
+                            "foregroundColor": {
+                                "opaqueColor": {
+                                    "rgbColor": {
+                                        "blue": 0.76,
+                                        "green": 0.482,
+                                        "red": 0.194
+                                    }
+                                }
+                            },
+                            "fontSize": {
+                                "unit": "PT",
+                                "magnitude": 8
+                            }
+                        },
+                        "textRange": {
+                            "type": "ALL"
+                        },
+                        "fields": "*",
+                        "objectId": str(textbox_reference.get(str(row[0]).strip().lower())) + ""
+                    }
+                }
+            ])
+            print('Made a tile Blighted')
+        # Apply the savage effect
+        if row[4] == "Savage":
+            default = False
+            requests.append([
+                {
+                    "updateTextStyle": {
+                        "style": {
+                            "bold": "true",
+                            "backgroundColor": {
+                                "opaqueColor": {
+                                    "rgbColor": {
+                                        "red": 0.874,
+                                        "green": 0.4,
+                                        "blue": 0.4
+                                    }
+                                }
+                            },
+                            "fontSize": {
+                                "unit": "PT",
+                                "magnitude": 8
+                            }
+                        },
+                        "objectId": str(textbox_reference.get(str(row[0]).strip().lower())) + "",
+                        "textRange": {
+                            "type": "ALL"
+                        },
+                        "fields": "*"
+                    }
+                }
+            ])
+            print('Made a tile Savage')
+        # Reset tiles with no effects to normal
+        if default:
+            requests.append([
+                {
+                    "updateTextStyle": {
+                        "style": {
+                            "fontSize": {
+                                "unit": "PT",
+                                "magnitude": 8
+                            }
+                        },
+                        "objectId": str(textbox_reference.get(str(row[0]).strip().lower())) + "",
+                        "textRange": {
+                            "type": "ALL"
+                        },
+                        "fields": "*"
+                    }
+                }
+            ])
+            print('Removed effects from a tile')
+    else:
+        print('Skipped an item with a null ID')
+print(requests)
+body = {
+        'requests': requests
+    }
+response = SLIDES.presentations().batchUpdate(presentationId=presentationID, body=body).execute()
