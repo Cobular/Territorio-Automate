75d2df37ddb4fd2b8d48636c60eefe141b8c514e
Jake Cover <coverj715@gmail.com>
2018-11-09 05:15:51 +0000

    Reorganized a bunch of stuff to make the code more legible and hit some better practices

--- sheets_to_slides_test.py
+++ sheets_to_slides_test.py
@@ -1,3 +1,11 @@
+#!./venv/bin python
+""" Provides a bunch of automation functions to be used with the game Territorio
+
+My game is hosted at SMHS by Mr. Terril.
+For this code to do anything other than crash,
+it requires the correct spreadsheet in the sheetID and presentationID fields.
+"""
+
 from __future__ import print_function
 
 from apiclient import discovery
@@ -5,21 +13,36 @@ from httplib2 import Http
 from oauth2client import file, client, tools
 import json
 
-# Assorted Variables
+# <editor-fold desc="Author Data">
+__author__ = "Jacob Cover"
+__credits__ = ["Jacob Cover"]
+
+__license__ = "GPL"
+__version__ = "1.2.1"
+__maintainer__ = "Jacob Cover"
+__email__ = "coverj715+territorio@gmail.com"
+# </editor-fold>
+
+# <editor-fold desc="Variable Definitions">
 # Creates the dict that will act as the reference in later parts of the code
+# tile_name: objectID
 textbox_reference = {}
-# Creates a dict that will store tile_name: [line_x,line_y]
+# Creates a dict that will store the spawnpoints for the tiles.
+# tile_name: [line_x,line_y]
 spawnpoint_reference = {}
-# Creates an empty list to reduce the number of requests sent. I was actually hitting the cap!.
-# This will do one request per run
+# Creates an empty list that all requests will be added to in order toreduce the number of requests sent.
+# I was actually hitting the cap! This will do one request per file run with the current setup.
+# Filled with tons of JSON stuff
 requests = []
+# Stores the ID of the sheet to get the data from
+sheetID = '1TcS0U8AdKQGcmAO1Hf4S8hrEs7BQ1aRqzOWFCx6o_ok'
+# Stores the ID of the presentation to apply the effects to
+presentationID = '1QFH68wLkhyfVag4c2JJ1tcEhgSIXhP5W0x-4vRbRrNY'
+# Stores the range of the data from the sheet to process here. Currently only works for one sheet tab at a time
+dataRange = 'A2:I172'
+# </editor-fold>
 
-# User defined variables
-sheetID = '1TcS0U8AdKQGcmAO1Hf4S8hrEs7BQ1aRqzOWFCx6o_ok'  # <--- Put the id of the sheet there
-presentationID = '1QFH68wLkhyfVag4c2JJ1tcEhgSIXhP5W0x-4vRbRrNY'  # <--- Put the id of the presentation
-dataRange = 'A2:I172'  # <--- Put the range of the data to process here. Currently only works for one sheet tab at a time
-
-# A ton of Authentication stuff from here to the next comment. Imporatnt but not very interesting
+# <editor-fold desc="Authentication Stuff">
 SCOPES = (
     'https://www.googleapis.com/auth/spreadsheets.readonly',
     'https://www.googleapis.com/auth/presentations'
@@ -32,7 +55,7 @@ if not creds or creds.invalid:
 HTTP = creds.authorize(Http())
 SLIDES = discovery.build('slides', 'v1', http=HTTP)
 SHEETS = discovery.build('sheets', 'v4', http=HTTP)
-
+# </editor-fold>
 
 # Actually get the sheet and slides values data from the API. Returns a JSON     file for each one.
 print('** Fetching Data, may take a bit')
@@ -43,11 +66,11 @@ requestedSlideValues = SLIDES.presentations().get(presentationId=presentationID)
 
 # This iterates over the list of page elements and only adds elements that have a text component to the
 # textbox_reference dict, which will be compared to the spreadsheet later
-for textboxes in requestedSlideValues[0]['pageElements']:
+for pageElement in requestedSlideValues[0]['pageElements']:
     try:
         textbox_reference.update(
-            {str(textboxes['shape']['text']['textElements'][1]['textRun']['content']).strip().lower():
-             str(textboxes['objectId']).strip().lower()}
+            {str(pageElement['shape']['text']['textElements'][1]['textRun']['content']).strip().lower():
+             str(pageElement['objectId']).strip().lower()}
         )
     except KeyError:
         pass
@@ -85,9 +108,12 @@ with open('JSON_for_testing/sheetsData_main.json', 'w') as sheetsDataFile:
 with open('JSON_for_testing/slidesData_main.json', 'w') as slidesDataFile:
     json.dump(requestedSlideValues, slidesDataFile, indent=4)
 
-# Iterate over the sheet and update the slide to match it.
-# Lots of JSON POST request bodies, so it's pretty messy-looking. Just collapse the requests.
+
 for row in requestedSheetValues:
+    """Iterate over the sheet and update the slide to match it
+    
+    Lots of JSON POST request bodies, so it's pretty messy-looking. Just collapse the requests or the whole thing.
+    """
     # Sets to false if anything is set to the tile.
     # If the tile is normal, this stays true and is used to set the tile to default at the end
     default = True
